4ce0bad7c1417ba0835345c45854af32121ae96e
Kumasilum\V.erS.us <vsgamesinc@ya.ru>
2017-09-26 22:57:37 +0300

    Create new Project

---
+++ MechBattleground/Source/MechBattleground/MechBattlegroundGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MechBattlegroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHBATTLEGROUND_API AMechBattlegroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
